2cbe3fd887a0e805539dc730270299f6d0f900ee
cmwschroeder <cmwschroeder@gmail.com>
2022-05-26 10:34:27 -0700

    added cache for images so that they will be loaded offline

--- client/src-sw.js
+++ client/src-sw.js
@@ -39,3 +39,19 @@ registerRoute(
       ],
     })
 );
+
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'my-image-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 10,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
